eb208e9994651581d1e357579ce5b269e95672a6
dorohi <m.m.dorohi@gmail.com>
2019-03-13 21:17:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', function () {
+	'use strict';
+	let info = document.querySelector('.info-header'),
+		tabs = document.querySelectorAll('.info-header-tab'),
+		tabContent = document.querySelectorAll('.info-tabcontent');
+	hideTabContent(1);
+	function hideTabContent (key){
+		for (let i = key; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		}
+	}
+	
+	function showTabContent(key){
+		if (tabContent[key].classList.contains('hide')){
+			tabContent[key].classList.remove('hide');
+			tabContent[key].classList.add('show');
+		}
+	}
+
+	info.addEventListener('click', (event) =>{
+		let target = event.target;
+		if (target && target.classList.contains('info-header-tab')) {
+			for (let i = 0; i < tabs.length; i++) {
+				if (tabs[i] == target){
+					hideTabContent(0);
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	});
+});
